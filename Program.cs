d9c3166a6411d8d6aa9c0abd87d2d5e297d6df33
Kombaiya <kombaiya4u@gmail.com>
2019-11-06 19:53:35 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace VersionChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1 ="1.4.0";
+            string input2 = "2.4.0.1";
+
+            var versionNumberObj = new VersionChecker();
+
+            if(!versionNumberObj.IsValidVersion(input1))
+            {
+                Console.WriteLine($"{input1} is not a valid version number");
+            }
+            else if(!versionNumberObj.IsValidVersion(input2))
+            {
+                Console.WriteLine($"{input2} is not a valid version number");
+            }
+            else
+            {
+                Console.WriteLine(versionNumberObj.CheckVersions(input1, input2));
+            }
+           
+        }
+    }
+}
---
+++ VersionChecker.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+
+namespace VersionChecker
+{
+    class VersionChecker
+    {
+        public string CheckVersions(string versionNumber1, string versionNumber2)
+        {
+            var number1 = Array.ConvertAll(versionNumber1.Split('.'), int.Parse);
+            var number2 = Array.ConvertAll(versionNumber2.Split('.'), int.Parse);
+            var smallerArray = number1.Length < number2.Length ? number1 : number2;
+
+            var outputString = string.Empty;
+
+         if(versionNumber1.Equals(versionNumber2))
+            {
+                 outputString = "equals";
+            }
+            else
+            {
+                for (int i = 0; i < smallerArray.Length; i++)
+                {
+                    if (number1[i] < number2[i])
+                    {
+                        outputString = "before";
+                        break;
+                    }
+                    else if (number1[i] > number2[i])
+                    {
+                        outputString = "after";
+                        break;
+                    }
+                }
+
+                if(string.IsNullOrEmpty(outputString))
+                {
+                    outputString = number1.Length > number2.Length ? "after" : "before";
+                }
+            }
+
+            return string.Format("{0} is \"{1}\" {2}", versionNumber1, outputString, versionNumber2);
+        }
+
+        public bool IsValidVersion(string versionNumber)
+        {
+            try
+            {
+                Version.Parse(versionNumber);
+                return true;
+            }
+            catch (Exception ex)
+            {
+               // Console.WriteLine(ex);
+                return false;
+            }
+        }
+    }
+}
